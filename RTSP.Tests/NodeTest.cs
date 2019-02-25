3e292aadff332ec82425314e96c44915012d777a
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-02-26 01:08:00 +1100

    Add some NUnit tests for testing Nodes

---
+++ RTSP.Tests/NodeTest.cs
@@ -0,0 +1,69 @@
+﻿namespace RTSP.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using RTSP.Core;
+
+    [TestFixture]
+    public class NodeTest
+    {
+        private Node parentNode;
+        private Node childNode;
+
+        /// <summary>
+        /// Initializes Node test objects
+        /// </summary>
+        /// 
+        [SetUp]
+        protected void SetUp()
+        {
+            parentNode = new TestParentNode();
+            childNode = new TestChildNode();
+
+            parentNode.AddChildren(childNode);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+        }
+
+        /// <summary>
+        /// Tests parent has child
+        /// </summary>
+        [Test]
+        public void TestChildOfParent()
+        {
+            var childrenOfParents = parentNode.Children;
+            Assert.IsNotNull(childrenOfParents);
+            var parentHasChild = childrenOfParents.TryGetValue(typeof(TestChildNode), out var childOfParent);
+            Assert.IsTrue(parentHasChild);
+            Assert.AreSame(childOfParent, childNode);
+        }
+
+        /// <summary>
+        /// Tests child has parent
+        /// </summary>
+        [Test]
+        public void TestParentOfChild()
+        {
+            var parentsOfChild = childNode.Parents;
+            Assert.IsNotNull(parentsOfChild);
+            var childHasParent = parentsOfChild.TryGetValue(typeof(TestParentNode), out var parentOfChild);
+            Assert.IsTrue(childHasParent);
+            Assert.AreSame(parentOfChild, parentNode);
+        }
+
+    }
+
+    class TestParentNode : Node
+    {
+
+    }
+
+    class TestChildNode : Node
+    {
+
+    }
+}
---
+++ RTSP.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RTSP.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RTSP.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("363c8b16-514e-4186-a45a-38df36aca734")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
