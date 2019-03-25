e55edcdf597bbaa49a3357d4c6c7b44b68812a12
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-03-26 00:33:59 +1100

    Conditionally read osu mods depending on Status node values

--- RTSP.Core/StatePresenter.cs
+++ RTSP.Core/StatePresenter.cs
@@ -16,7 +16,7 @@ namespace RTSP.Core
         private readonly List<Action<State>> _eventHandlers_NewState;
 
         // TODO: Load this from config
-        private TimeSpan _scannerInterval = TimeSpan.FromMilliseconds(50);
+        private TimeSpan _scannerInterval = TimeSpan.FromMilliseconds(250);
 
         public StatePresenter()
         {
