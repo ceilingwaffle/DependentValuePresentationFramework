28007a471c2c251d8d6fbd36e86f7d2f084699b5
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-02 00:03:10 +1100

    Replace SP scanner loop with ActionBlock

--- DVPF.Core/StatePresenter.cs
+++ DVPF.Core/StatePresenter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace DVPF.Core
 {
@@ -15,6 +17,9 @@ namespace DVPF.Core
         private readonly StateBuilder _stateBuilder;
         private readonly List<Action<State>> _eventHandlers_NewState;
 
+        private ITargetBlock<DateTimeOffset> _scannerTask;
+        private CancellationTokenSource _scannerTaskCTS;
+
         public StatePresenter()
         {
             LogAllExceptions();
@@ -23,86 +28,168 @@ namespace DVPF.Core
             _eventHandlers_NewState = new List<Action<State>>();
         }
 
-        public async Task StartScannerLoopAsync()
+        public void StartScannerLoop()
         {
             NodeSupervisor.BuildNodeCollections();
 
-            while (true)
+            // Create the token source.
+            _scannerTaskCTS = new CancellationTokenSource();
+
+            // TODO: Optimize - fix delay sometimes between states being created
+
+            // Set the task.
+            _scannerTask = CreateScannerInfiniteLoopTask((now, ct) => DoWorkAsync(ct), _scannerTaskCTS.Token);
+
+            // Start the task.  Post the time.
+            _scannerTask.Post(DateTimeOffset.Now);
+        }
+
+        public void StopScannerLoop()
+        {
+            // CancellationTokenSource implements IDisposable.
+            using (_scannerTaskCTS)
             {
-                _logger.Debug("------------------------------------------------------");
-
-                //var leafNodes = NodeSupervisor.LeafNodes;
-                var presentationEnabledNodes = NodeSupervisor.GetEnabledNodes();
-
-                var tasks = new List<Task>();
-
-                foreach (var node in presentationEnabledNodes)
-                {
-                    //if (node.TaskManager.GetUpdateTaskStatus() != TaskStatus.Running)
-                    //{
-                    //    node.TaskManager.DisposeUpdateTask();
-                    //    node.TaskManager.ResetUpdateTaskCTS();
-                    //}
-
-                    var task = node.TaskManager.UpdateAsync();
-
-                    tasks.Add(task);
-                }
-
-                try
-                {
-                    await Task.WhenAny(tasks);
-                }
-                catch (AggregateException ae)
-                {
-                    _logger.Error(ae.Message);
-
-                    foreach (var e in ae.InnerExceptions)
-                    {
-                        _logger.Error(e.Message);
-                    }
-                }
-                catch (TaskCanceledException tce)
-                {
-                    _logger.Error(tce.Message);
-                }
-                catch (OperationCanceledException oce)
-                {
-                    _logger.Error(oce.Message);
-                }
-
-                // TODO: Fix unnecessary delay due to execution time + delay time (delay time should be reduced by the execution time, but still maintain some minimum delay time so the tasks don't go haywire)
-                await Task.Delay(ScannerInterval);
-
-                // TODO: Shift the "latest" value to "previous" and copy each value on the state to "latest" for each node.
-                //       This is to prevent situations where e.g. MapTime from having equal "current" and "previous" values despite the current and previous values on the State having different values.
-
-                // do its best to set as many Node values as possible on the State (not all Nodes will have finished updating yet).
-                // build the state
-                State state = _stateBuilder.Build();
-
-                // pass the state to the event handlers
-                ProcessEventHandlers_NewStateCreated(state);
-
-                // reset all Node update tasks
-                foreach (var node in Node.InitializedNodes)
-                {
-                    TaskStatus taskStatus = node.TaskManager.GetUpdateTaskStatus();
-
-                    if (taskStatus == TaskStatus.WaitingToRun // when null
-                        || taskStatus == TaskStatus.Canceled
-                        || taskStatus == TaskStatus.RanToCompletion
-                        || taskStatus == TaskStatus.Faulted
-                    )
-                    {
-                        _logger.Debug($"{node.T()} Resetting _updateTask (task status: {taskStatus.ToString()})");
-
-                        node.TaskManager.DisposeUpdateTask();
-                        node.TaskManager.ResetUpdateTaskCTS();
-                    }
-                }
-
+                // Cancel.  This will cancel the task.
+                _scannerTaskCTS.Cancel();
             }
+
+            // Set everything to null, since the references
+            // are on the class level and keeping them around
+            // is holding onto invalid state.
+            _scannerTaskCTS = null;
+            _scannerTask = null;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/13712646
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private ITargetBlock<DateTimeOffset> CreateScannerInfiniteLoopTask(Func<DateTimeOffset, CancellationToken, Task> action, CancellationToken cancellationToken)
+        {
+            // Validate parameters.
+            if (action == null) throw new ArgumentNullException("action");
+
+            // Declare the block variable, it needs to be captured.
+            ActionBlock<DateTimeOffset> block = null;
+
+            // Create the block, it will call itself, so
+            // you need to separate the declaration and
+            // the assignment.
+            // Async so you can wait easily when the
+            // delay comes.
+            block = new ActionBlock<DateTimeOffset>(async now =>
+            {
+                // Perform the action.  Wait on the result.
+                await action(now, cancellationToken).
+                    // Doing this here because synchronization context more than
+                    // likely *doesn't* need to be captured for the continuation
+                    // here.  As a matter of fact, that would be downright
+                    // dangerous.
+                    ConfigureAwait(false);
+
+                // Wait.
+                await Task.Delay(ScannerInterval, cancellationToken).
+                    // Same as above.
+                    ConfigureAwait(false);
+
+                // Post the action back to the block.
+                block.Post(DateTimeOffset.Now);
+
+                _ProcessState();
+                _ResetNodeUpdateTasks();
+
+            }, new ExecutionDataflowBlockOptions
+            {
+                CancellationToken = cancellationToken
+            });
+
+            // Return the block.
+            return block;
+        }
+
+        private static void _ResetNodeUpdateTasks()
+        {
+            // reset all Node update tasks
+            foreach (var node in Node.InitializedNodes)
+            {
+                TaskStatus taskStatus = node.TaskManager.GetUpdateTaskStatus();
+
+                if (taskStatus == TaskStatus.WaitingToRun // when null
+                    || taskStatus == TaskStatus.Canceled
+                    || taskStatus == TaskStatus.RanToCompletion
+                    || taskStatus == TaskStatus.Faulted
+                )
+                {
+                    _logger.Debug($"{node.T()} Resetting _updateTask (task status: {taskStatus.ToString()})");
+
+                    node.TaskManager.DisposeUpdateTask();
+                    node.TaskManager.ResetUpdateTaskCTS();
+                }
+            }
+        }
+
+        private void _ProcessState()
+        {
+            // TODO: Fix unnecessary delay due to execution time + delay time (delay time should be reduced by the execution time, but still maintain some minimum delay time so the tasks don't go haywire)
+            //await Task.Delay(ScannerInterval);
+
+            // TODO: Shift the "latest" value to "previous" and copy each value on the state to "latest" for each node.
+            //       This is to prevent situations where e.g. MapTime from having equal "current" and "previous" values despite the current and previous values on the State having different values.
+
+            // do its best to set as many Node values as possible on the State (not all Nodes will have finished updating yet).
+            // build the state
+            State state = _stateBuilder.Build();
+
+            // pass the state to the event handlers
+            ProcessEventHandlers_NewStateCreated(state);
+        }
+
+        private async Task DoWorkAsync(CancellationToken cancellationToken)
+        {
+            _logger.Debug("------------------------------------------------------");
+
+            //var leafNodes = NodeSupervisor.LeafNodes;
+            var presentationEnabledNodes = NodeSupervisor.GetEnabledNodes();
+
+            var tasks = new List<Task>();
+
+            foreach (var node in presentationEnabledNodes)
+            {
+                //if (node.TaskManager.GetUpdateTaskStatus() != TaskStatus.Running)
+                //{
+                //    node.TaskManager.DisposeUpdateTask();
+                //    node.TaskManager.ResetUpdateTaskCTS();
+                //}
+
+                var task = node.TaskManager.UpdateAsync();
+
+                tasks.Add(task);
+            }
+
+            try
+            {
+                await Task.WhenAny(tasks);
+            }
+            catch (AggregateException ae)
+            {
+                _logger.Error(ae.Message);
+
+                foreach (var e in ae.InnerExceptions)
+                {
+                    _logger.Error(e.Message);
+                }
+            }
+            catch (TaskCanceledException tce)
+            {
+                _logger.Error(tce.Message);
+            }
+            catch (OperationCanceledException oce)
+            {
+                _logger.Error(oce.Message);
+            }
+
 
         }
 
