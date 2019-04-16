3bf7b19dcb6471282248b17884c597fcd5761dae
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-16 19:30:03 +1000

    Implement Stylecop suggestions for NodeEventArgs

--- DVPF.Core/NodeEventArgs.cs
+++ DVPF.Core/NodeEventArgs.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DVPF.Core
 {
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Event data for <see cref="Node"/> state changes.
+    /// </summary>
     public class NodeEventArgs : EventArgs
     {
-        public object Value { get; set; }
-
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DVPF.Core.NodeEventArgs" /> class.
+        /// </summary>
+        /// <param name="value">
+        /// The value of <see cref="T:DVPF.Core.Node" />
+        /// </param>
         public NodeEventArgs(object value)
         {
-            Value = value;
+            this.Value = value;
         }
+
+        /// <summary>
+        /// Gets or sets the value of the <see cref="Node"/>
+        /// </summary>
+        public object Value { get; set; }
+
     }
 }
